8f079eb9fdec90120a12ce32926abf90bec374f4
Andres Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-06-02 21:53:15 -0500

    Imagene n post
    Se aadió imagen a los posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,25 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\Storage;
+
 class PostController extends Controller
 {
     public function createPost()
-    {       
+    {
         return view('/publication/post');
     }
 
-    public function storePost(Request $request){
-        
-        $post= Post::create([
-            'user_id' =>Auth::id(),
-            'tittle'=> $request->tittle,
-            'typePet'=> $request->typePet,
-            'location'=> $request->location,
-            'picture'=>'hola',
-            'description'=> $request->description,
+    public function storePost(Request $request)
+    {
+
+        //IMAGE
+        $path = Storage::disk('public')->put('image', $request->file('picture'));
+
+        $post = Post::create([
+            'user_id' => Auth::id(),
+            'tittle' => $request->tittle,
+            'typePet' => $request->typePet,
+            'location' => $request->location,
+            'picture' => $path,
+            'description' => $request->description,
         ]);
 
         return redirect()->back()->with('success', "El post: $post->tittle ya fue creado con exito.");
--- resources/views/ViewPost.blade.php
+++ resources/views/ViewPost.blade.php
@@ -1,28 +1,27 @@
-
 @extends('layouts.app')
 
 @section('content')
 
-<div class='container'>
-<a href="{{ url('/publication/post') }}" class="btn btn-success mb-5" >Crear Post</a>
+    <div class='container'>
+        <a href="{{ url('/publication/post') }}" class="btn btn-success mb-5">Crear Post</a>
 
-<div class=row>
-@foreach($posts as $post)
-  <div class="col-4 mt-5">
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-           <h5 class="card-title">{{$post->tittle}}</h5>
-           <p class="card-text">{{$post->typePet}}</p>
-           <p class="card-text">{{$post->description}}</p>
-           <p class="card-text">{{$post->location}}</p>
-           <p class="card-text">{{$post->description}}</p>
+        <div class=row>
+            @foreach($posts as $post)
+                <div class="col-4 mt-5">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $post->picture }}" class="card-img-top" alt="{{ $post->picture }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->tittle }}</h5>
+                            <p class="card-text">{{ $post->typePet }}</p>
+                            <p class="card-text">{{ $post->description }}</p>
+                            <p class="card-text">{{ $post->location }}</p>
+                            <p class="card-text">{{ $post->description }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
-  </div>
-@endforeach
-</div>
-</div>
 
 
 @endsection
